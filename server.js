00f3573027c816f7183656e1ff4c830da7b407f6
Drew Olsen <drew@drewolsendesign.com>
2021-04-08 15:00:38 -0500

    added some packages and base files

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = 8000;
+
+// NOTE: This is what allows static things like stylesheets
+app.use(express.static('public'));
