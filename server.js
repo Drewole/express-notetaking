3dcb39cd2e1f5098d153bba4c4299e6fcdd41de4
Drew Olsen <olsenj.andrew@gmail.com>
2021-04-09 17:08:02 -0500

    Trying to get this reading file stuff working.

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const { nanoid } = require('nanoid');
 const fs = require('fs');
 const path = require('path');
 
 const app = express();
 
-const allNotes = [];
+var allNotes = [];
 
 // IF this is a production env use that port, or if thats not there use 8000
 const PORT = process.env.PORT || 8000;
@@ -25,21 +26,59 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
-app.get('/api/characters/all', (req, res) => {
-  if( obj === undefined ){
+// app.get('/api/notes', (req, res) => res.json(allNotes));
+
+app.get('/api/notes', (req, res) => {
+  
+  if( res === undefined ){
     console.log("bad route")
     res.send("You done messed up, Aaron");
   } else {
-    console.log(obj);
-    res.json(obj)
+    console.log("This worked");
+
+    readFile(res);
+    res.json(allNotes)
   }
-});
+  
+ });
 
 app.post('/api/notes', (req, res) => {
-  console.log(req.body);
-  characters.push(req.body);
-  res.json(req.body);
-});
+  // Lets assign the note deets to a new const
+  const newNote = req.body;
+  // Use the nano ID package to assign a 4 integer unique ID
+  newNote.id = nanoid(4);
 
+  // Add the new note
+  allNotes.push(newNote);
+  // Send the notes back to the client
+  res.json(allNotes);
+
+  allNotes = JSON.stringify(allNotes);
+
+  // This is where we will read data from the database.
+    fs.writeFile('./db/db.json', allNotes, (err,data) => {
+      if (err) throw err;
+      let theData = data;
+      console.log(theData)
+
+      allNotes = theData;
+      
+    });
+    // Lets push the new note into the local array
+});
+// function writeFile(data) {
+//     // This is where we will write the data
+// }
+function readFile(res) {
+    // This is where we will read data from the database.
+    fs.readFile('./db/db.json', (err,data) => {
+      if (err) throw err;
+      let theData = JSON.parse(data);
+      console.log(theData)
+
+      allNotes = theData;
+      
+    });
+}
 // Listener
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
