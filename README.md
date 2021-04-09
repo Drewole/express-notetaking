d721ec53f4db16a8eaa244f786ca57ac93fc6c43
Drew Olsen <olsenj.andrew@gmail.com>
2021-04-09 01:03:20 -0500

    Generated a base readme file.

---
+++ README.md
@@ -0,0 +1,47 @@
+
+
+# Note Taking App
+========================
+
+
+![Screenshot or Gif of the app.](./assets/screenshot.png)
+
+### Description
+Simple note taking application using express as a router.
+
+
+## Table of Contents
+* [Installation](#Installation)
+* [Usage](#Usage-Information)
+* [License](#License)
+* [Contribution Guidelines](#Contribution-Guidelines)
+* [Test Instructions](#Test-Instructions)
+* [Contact](#Contact-Information)
+
+### Installation
+Download
+
+Customize server.js to your liking
+
+run npm install
+
+run node server.js to start the app
+
+
+### Usage Information
+None
+
+### License Information
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+### Contribution Guidelines
+None
+
+### Test Instructions
+None
+ 
+### Contact Information
+[Email Me](olsenj.andrew@gmail.com)
+
+[Github](https://github.com/Drewole)
+
--- server.js
+++ server.js
@@ -3,7 +3,43 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 8000;
 
-// NOTE: This is what allows static things like stylesheets
+const allNotes = [];
+
+// IF this is a production env use that port, or if thats not there use 8000
+const PORT = process.env.PORT || 8000;
+
+// NOTE: This is what allows static things like stylesheets, js files and html files to be found and loaded
 app.use(express.static('public'));
+// NOTE: This is what allows us to receive form data
+app.use(express.urlencoded({ extended: true }));
+// NOTE: I think this is where we decide what format to move information around in.
+app.use(express.json());
+
+////////////////// Routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+app.get('/api/characters/all', (req, res) => {
+  if( obj === undefined ){
+    console.log("bad route")
+    res.send("You done messed up, Aaron");
+  } else {
+    console.log(obj);
+    res.json(obj)
+  }
+});
+
+app.post('/api/notes', (req, res) => {
+  console.log(req.body);
+  characters.push(req.body);
+  res.json(req.body);
+});
+
+// Listener
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
